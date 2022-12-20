95d94711692bd9984db32240871760f7e5ccda87
Ashish Kumar <98329891+aashishh121@users.noreply.github.com>
2022-12-20 10:32:21 +0530

    Add files via upload

---
+++ src/features/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import AddUser from "./features/users/AddUser";
+import EditUser from "./features/users/EditUser";
+import UserList from "./features/users/UserList";
+
+function App() {
+  return (
+    <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/add-user" element={<AddUser />} />
+        <Route path="/edit-user/:id" element={<EditUser />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
